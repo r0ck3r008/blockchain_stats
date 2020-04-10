52a52817c0480202a723c627c95478130bae89c3
r0ck3r008 <naman.arora@tutanota.com>
2020-04-10 19:51:13 -0400

    updated query 4

--- src/queries/q4.sql
+++ src/queries/q4.sql
@@ -1,34 +1,50 @@
 /* question 4
- avg number of input/output txs per address */
-with naddresses as (
-	select count(*) as totaladdresses
-	from addresses
-)
-select	(sum(c.outtxs)/(
-		select totaladdresses
-		from naddresses
-)) as avgouttxs,
-	(sum(c.intxs)/(
-			select totaladdresses
-			from naddresses
-	)) as averageintxs,
-(sum(c.totaltxs)/(
-		select totaladdresses
-		from naddresses
-)) as averagetotaltxs
-from (
-	select a.outtxs as outtxs, b.intxs as intxs, (a.outtxs+b.intxs) as totaltxs
+avg number of input/output txs per address
+cte1 gets the count of addresses involved in contributing a transaction as input.
+cte2 gets the count of addresses that ever received a bitcoin through a transaction.
+cte3 gets the total input count of transactions
+cte4 gets the total output count of transactions.
+We then simply average out cte3/cte1 for input tx avg, cte4/cte2 for output tx avg
+and cte3+cte4/total addresses for total tx avg */
+with cte1 as (
+	select count(*) as total_in_addrs
 	from (
-		select addrid, count(*) as outtxs
-		from txout
-		group by addrid
-		having addrid != -1
-	) a
-	join (
-		select addrid, count(*) as intxs
+		select distinct addrid
 		from txin
-		group by addrid
-		having addrid != -1
-	) b
-	on a.addrid=b.addrid
-) c;
+	) a
+), cte2 as (
+	select count(*) as total_out_addrs
+	from (
+		select distinct addrid
+		from txout
+	) a
+), cte3 as (
+	select sum(n_inputs) as total_in
+	from tx
+), cte4 as (
+	select sum(n_outputs) as total_out
+	from tx
+)
+select 'Average input txs per address', cte3.total_in/(
+	select total_in_addrs
+	from cte1
+)
+from cte3
+union
+select 'Average output txs per address', cte4.total_out/(
+	select total_out_addrs
+	from cte2
+)
+from cte4
+union
+select 'Average txs per address', (
+	(
+		select total_in
+		from cte3
+	) +
+	(
+		select total_out
+		from cte4
+	)
+)/count(*)
+from addresses;
