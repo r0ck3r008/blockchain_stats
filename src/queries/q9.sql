d7e09813e3671e287d45b08d98e37a7034a646cc
r0ck3r008 <naman.arora@tutanota.com>
2020-04-10 18:36:26 -0400

    added query that calculates distinct users in users table

--- src/queries/q9.sql
+++ src/queries/q9.sql
@@ -1,5 +1,7 @@
-SELECT addresses.addrID as AddrID, COUNT(txin.txID) as TxIn
-FROM addresses
-JOIN txin
-ON addresses.addrID=txin.addrID
-GROUP BY addresses.addrID;
+/* question 9
+distinct users in database with joint+serial control */
+select count(*)
+from (
+	select distinct userid
+	from users
+) a;
