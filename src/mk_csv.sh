d27b8a55260768c717f1af7a49229b156f54c96f
r0ck3r008 <naman.arora@tutanota.com>
2020-04-10 20:01:15 -0400

    updated mkcsv.sh and removed txedges

--- src/mk_csv.sh
+++ src/mk_csv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-g++ -g -o txedge txedges/txedge.cpp -std=gnu++14 -O3 -march=native
+g++ -o sccs32s sccs32s/sccs32s.cpp -std=gnu++14 -O3 -march=native
 
 zcat "${1}"/bh.dat.gz | head -n 212576 > dataset/bh.dat
 zcat "${1}"/txh.dat.gz | head -n 10000055 > dataset/txh.dat
@@ -8,9 +8,3 @@ zcat "${1}"/addresses.dat.gz | head -n 8385065 > dataset/addresses.dat
 xzcat "${1}"/tx.dat.xz | head -n 10000055 > dataset/tx.dat
 xzcat "${1}"/txin.dat.xz | head -n 20168554 > dataset/txin.dat
 xzcat "${1}"/txout.dat.xz | head -n 23266807 > dataset/txout.dat
-#zcat multiple.dat.gz | sed "s/\t/,/g" | > dataset/multiple.dat
-#zcat nonstandard.dat.gz | sed "s/\t/,/g" | > dataset/nonstandard.dat
-#zcat addr_sccs.dat.gz | sed "s/\t/,/g" | > dataset/addr_sccs.dat
-
-./txedge -ip dataset/txin.dat -op dataset/txout.dat > dataset/tx_edges.dat
-rm -f ./txedge
