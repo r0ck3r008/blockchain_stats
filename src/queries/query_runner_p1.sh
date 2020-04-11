9a851456e6ec1b3e2595dd62441b78317cac33c1
r0ck3r008 <naman.arora@tutanota.com>
2020-04-10 20:05:34 -0400

    added runner script for part 1

---
+++ src/queries/query_runner_p1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+psql --username postgres -a -f "${1}"/q1.sql
+psql --username postgres -a -f "${1}"/q2.sql
+psql --username postgres -a -f "${1}"/q3.sql
+psql --username postgres -a -f "${1}"/q4.sql
+psql --username postgres -a -f "${1}"/q5.sql
+psql --username postgres -a -f "${1}"/q6.sql
+psql --username postgres -a -f "${1}"/q7.sql
+psql --username postgres -a -f "${1}"/q8.sql
